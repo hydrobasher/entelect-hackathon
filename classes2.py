db9c4b87266c215a286820614ae887d1444f0359
Hydrobasher <luanreyneke@gmail.com>
2026-04-18 11:10:42 +0200

    write

--- classes2.py
+++ classes2.py
@@ -1,7 +1,11 @@
 class outSegment:
-    def __init__(self, id, level, segment):
+    def __init__(self, id, segment):
         self.id = id
         self.type = segment.type
+
+class outPit:
+    def __init__(self):
+        self.enter = False      
 
 class outLap:
     def __init__(self, lap, level):
@@ -12,13 +16,15 @@ class outLap:
             segments.append(outSegment(i+1, level.track.segments[i]))
         self.segments = segments
 
+        self.pit = outPit()
+
 class outLevel:
     def __init__(self, level):
         self.level = level
+
+        self.initial_tyre_id = 1
 
         laps = []
         for i in range(self.level.race.laps):
             laps.append(outLap(i+1, self.level))
         self.laps = laps
-
-        initial_tyre_id = 1
